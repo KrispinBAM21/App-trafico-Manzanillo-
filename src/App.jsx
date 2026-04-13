e2a4dfaf79f28b83c57c91648c4b6cb0b6c925b9
KrispinBAM21 <owldanger@hotmail.com>
2026-04-13 14:17:10 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7866,8 +7866,7 @@ function App() {
               Cualquier monto es apreciado 💜<br/>
               ¡Gracias por tu apoyo!
             </div>
-         </div>
-)
+        </div>
 </div>
 </div>
 </div>
